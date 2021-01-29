b97e88df057415943c53887eb43973994be8d970
Alex Novikov <l.novicovdean@gmail.com>
2021-01-29 15:16:11 +0300

    prod build

--- resources/js/store/configureStore.js
+++ resources/js/store/configureStore.js
@@ -1,12 +1,12 @@
 import {configureStore, getDefaultMiddleware} from '@reduxjs/toolkit'
 import rootReducer from '../redux/reducers'
-import {createLogger} from 'redux-logger'
+// import {createLogger} from 'redux-logger'
 import {apiMiddleware} from '../redux/enhancers/apiEnhancers'
 
 export default function configureAppStore(preloadedState) {
     return configureStore({
         reducer: rootReducer(),
-        middleware: [createLogger(), apiMiddleware, ...getDefaultMiddleware({serializableCheck: false})],
+        middleware: [/*createLogger(), */apiMiddleware, ...getDefaultMiddleware({serializableCheck: false})],
         preloadedState,
     })
 }
